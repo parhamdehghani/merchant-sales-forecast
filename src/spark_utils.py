fd97775de4571c9d36e1ba6a872303b9fd946cc5
Parham <p.dehghani88@gmail.com>
2025-09-23 15:25:08 -0400

    spark initialization locally

---
+++ src/spark_utils.py
@@ -0,0 +1,30 @@
+from pyspark.sql import SparkSession
+from pyspark.conf import SparkConf
+
+def get_or_create_spark_session(app_name="MerchantSalesForecast") -> SparkSession:
+    """
+    Gets an existing SparkSession or creates a new one if it doesn't exist.
+    Configures Spark for optimal performance and local execution.
+
+    Args:
+        app_name (str): The name of the Spark application.
+
+    Returns:
+        SparkSession: An instance of SparkSession.
+    """
+    conf = SparkConf() \
+        .setAppName(app_name) \
+        .set("spark.executor.memory", "2g") \
+        .set("spark.driver.memory", "4g") \
+        .set("spark.sql.shuffle.partitions", "200")
+
+    # For local development, use 'local[*]' master
+    # In a production Docker environment, this might be overridden by a Kubernetes/YARN master
+    # For this assessment, 'local[*]' is appropriate.
+    conf = conf.setMaster("local[*]")
+
+    spark = SparkSession.builder \
+        .config(conf=conf) \
+        .getOrCreate()
+
+    return spark
